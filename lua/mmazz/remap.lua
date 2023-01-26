0721659f116218dcfb32a86e64fc84cf522a0a8a
Matias Mazzanti <mazzantimatiass@gmail.com>
2023-01-26 10:25:09 -0300

    Purificando

--- lua/mmazz/remap.lua
+++ lua/mmazz/remap.lua
@@ -25,7 +25,7 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- This is going to get me cancelled
