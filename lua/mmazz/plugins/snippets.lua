e084e160f7c2ff4eb690ec6fc6ce58ea6ce9bbfc
mmazz <mazzantimatiass@gmail.com>
2024-01-26 17:54:35 -0300

    Falta arreglar luasnip

--- lua/mmazz/plugins/snippets.lua
+++ lua/mmazz/plugins/snippets.lua
@@ -1,8 +1,11 @@
 return {
     {
         "L3MON4D3/LuaSnip",
-        version = "v2.*",
+        	-- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
         build = "make install_jsregexp",
+
         config = function()
             local ls = require("luasnip")
 
